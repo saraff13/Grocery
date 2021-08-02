e3067ed9424b5c5e7e47804d5d0a8150c12dd4e9
Sumit Kumar <KumarSumit@XEBIAINDIA.LOCAL>
2021-08-02 22:24:41 +0530

    custom drawer and styling

--- src/navigations/DrawerNavigator.js
+++ src/navigations/DrawerNavigator.js
@@ -15,7 +15,9 @@ import Birthday from '../scenes/Birthday/Birthday';
 import ShowBirthdayItemList from '../scenes/Birthday/ShowBirthdayItemList';
 import {connect} from 'react-redux';
 import {responsiveWidth} from '../utils/Responsive';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+const Icon = MaterialCommunityIcons;
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 
@@ -40,12 +42,53 @@ class DrawerNavigator extends Component {
     const {user} = this.props;
     return (
       <Drawer.Navigator
+        drawerStyle={{
+          width: responsiveWidth(55),
+          backgroundColor: 'aliceblue',
+        }}
+        drawerContentOptions={{
+          activeTintColor: 'white',
+          activeBackgroundColor: 'steelblue',
+          inactiveTintColor: 'black',
+          itemStyle: {marginVertical: 0},
+          labelStyle: {fontSize: 18},
+        }}
         drawerContent={props => <CustomDrawerContent user={user} {...props} />}>
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="Birthday" component={myBirthdayPlans} />
-        <Drawer.Screen name="Profile" component={Profile} />
-        <Drawer.Screen name="About" component={About} />
-        <Drawer.Screen name="Help" component={Help} />
+        <Drawer.Screen
+          name="Home"
+          component={Home}
+          options={{
+            drawerIcon: () => <Icon name="home" size={18} />,
+          }}
+        />
+        <Drawer.Screen
+          name="Birthday"
+          component={myBirthdayPlans}
+          options={{
+            drawerIcon: () => <Icon name="cake-variant" size={18} />,
+          }}
+        />
+        <Drawer.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            drawerIcon: () => <Icon name="account" size={18} />,
+          }}
+        />
+        <Drawer.Screen
+          name="About"
+          component={About}
+          options={{
+            drawerIcon: () => <Icon name="information" size={18} />,
+          }}
+        />
+        <Drawer.Screen
+          name="Help"
+          component={Help}
+          options={{
+            drawerIcon: () => <Icon name="help-circle" size={18} />,
+          }}
+        />
       </Drawer.Navigator>
     );
   }
@@ -88,15 +131,18 @@ const styles = StyleSheet.create({
   profileBox: {
     alignItems: 'center',
     marginVertical: 20,
+    // borderWidth: 1,
+    marginBottom: 15,
   },
   image: {
-    width: responsiveWidth(25),
-    height: responsiveWidth(25),
+    width: responsiveWidth(27),
+    height: responsiveWidth(27),
     borderRadius: 200,
-    marginVertical: 10,
+    marginBottom: 5,
   },
   name: {
-    fontSize: 22,
+    fontSize: 25,
     fontWeight: 'bold',
+    // borderWidth: 1,
   },
 });
